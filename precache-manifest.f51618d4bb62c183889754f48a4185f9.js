76ca9208d12ecee6d54db55d6bf125d6526b9b46
cmcg279 <cmcg278@gmail.com>
2020-05-14 17:46:32 +0100

    Updates

---
+++ precache-manifest.f51618d4bb62c183889754f48a4185f9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dbf465198ead64787c7c2637225ffb0",
+    "url": "/cmcg279/nibjj/index.html"
+  },
+  {
+    "revision": "14888e09bc5e3c78b7cc",
+    "url": "/cmcg279/nibjj/static/css/2.8d24d6c7.chunk.css"
+  },
+  {
+    "revision": "7901649aee1d32bb75bc",
+    "url": "/cmcg279/nibjj/static/css/main.abd634f7.chunk.css"
+  },
+  {
+    "revision": "14888e09bc5e3c78b7cc",
+    "url": "/cmcg279/nibjj/static/js/2.7273c072.chunk.js"
+  },
+  {
+    "revision": "4275c1d12d633733c6fec0905104cb9a",
+    "url": "/cmcg279/nibjj/static/js/2.7273c072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7901649aee1d32bb75bc",
+    "url": "/cmcg279/nibjj/static/js/main.f4f47a26.chunk.js"
+  },
+  {
+    "revision": "4af323fe3b6ca7aae806",
+    "url": "/cmcg279/nibjj/static/js/runtime-main.6f18e045.js"
+  },
+  {
+    "revision": "aba6ebc16f0eb4d8c1d61fd8fbe2f3c7",
+    "url": "/cmcg279/nibjj/static/media/armbar.aba6ebc1.jpg"
+  },
+  {
+    "revision": "811c13d3caebc5f512030439a5363f18",
+    "url": "/cmcg279/nibjj/static/media/nibjj_yellow.811c13d3.jpg"
+  },
+  {
+    "revision": "dc385185c9923766b2e78559de583f41",
+    "url": "/cmcg279/nibjj/static/media/smart-man-chair.dc385185.jpg"
+  },
+  {
+    "revision": "1f7bb7c62bc1557e5a3867cdea61c449",
+    "url": "/cmcg279/nibjj/static/media/triangle.1f7bb7c6.jpg"
+  }
+]);
