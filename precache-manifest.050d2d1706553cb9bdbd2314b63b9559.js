a53a17b34cdcc7161f5b0c60e0d3866ec4c264fe
cmcg279 <cmcg278@gmail.com>
2020-05-14 17:51:57 +0100

    Updates

---
+++ precache-manifest.050d2d1706553cb9bdbd2314b63b9559.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7ec6fa88b188712e0c972a35c324b5b",
+    "url": "/nibjj/index.html"
+  },
+  {
+    "revision": "14888e09bc5e3c78b7cc",
+    "url": "/nibjj/static/css/2.8d24d6c7.chunk.css"
+  },
+  {
+    "revision": "9ef3ad48aa305c6997e7",
+    "url": "/nibjj/static/css/main.abd634f7.chunk.css"
+  },
+  {
+    "revision": "14888e09bc5e3c78b7cc",
+    "url": "/nibjj/static/js/2.7273c072.chunk.js"
+  },
+  {
+    "revision": "4275c1d12d633733c6fec0905104cb9a",
+    "url": "/nibjj/static/js/2.7273c072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ef3ad48aa305c6997e7",
+    "url": "/nibjj/static/js/main.18d2e992.chunk.js"
+  },
+  {
+    "revision": "ef434379ffde37a9bba6",
+    "url": "/nibjj/static/js/runtime-main.15aebbcb.js"
+  },
+  {
+    "revision": "aba6ebc16f0eb4d8c1d61fd8fbe2f3c7",
+    "url": "/nibjj/static/media/armbar.aba6ebc1.jpg"
+  },
+  {
+    "revision": "811c13d3caebc5f512030439a5363f18",
+    "url": "/nibjj/static/media/nibjj_yellow.811c13d3.jpg"
+  },
+  {
+    "revision": "dc385185c9923766b2e78559de583f41",
+    "url": "/nibjj/static/media/smart-man-chair.dc385185.jpg"
+  },
+  {
+    "revision": "1f7bb7c62bc1557e5a3867cdea61c449",
+    "url": "/nibjj/static/media/triangle.1f7bb7c6.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cmcg279/nibjj/precache-manifest.f51618d4bb62c183889754f48a4185f9.js"
+  "/nibjj/precache-manifest.050d2d1706553cb9bdbd2314b63b9559.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cmcg279/nibjj/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nibjj/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
